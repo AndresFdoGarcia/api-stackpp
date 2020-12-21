f5ccaaa7231c00d6c18b9e614d91ffb593268320
AndresFdoGarcia <andres.perea@hotmail.com>
2020-12-21 09:24:49 -0500

    agregando detalles

--- models/user_models.py
+++ models/user_models.py
@@ -4,6 +4,8 @@ class UserIn(BaseModel):
     username: str
     password: str
 class UserOut(BaseModel):
+    nombre: str
+    apellido: str
     username: str
     balance: int
 class GetUser(BaseModel):
